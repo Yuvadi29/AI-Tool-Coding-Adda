d348b369a0e62c1b79d9d1bf85bf9e45022099fc
Yuvadi29 <adityatrivedi612@gmail.com>
2025-04-09 17:51:53 +0530

    Adding POST route for API call

---
+++ app/api/generate/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const { url } = await req.json()
+
+    try {
+        // Fetch Transcript
+        let transcript = await fetchTranscript(url);
+
+        //Detect language
+        const language = await detectLanguage(transcript);
+
+        //Translate if Hindi
+        const translatedTranscript = language === "hi" ? await translateToEnglish(transcript) : transcript;
+
+        // Run AI tasks
+        const aiOutput = await runAITasks(translatedTranscript);
+
+        // Save everything to DB
+        await saveToDB({
+            url,
+            transcript,
+            translatedTranscript,
+            language,
+            ...aiOutput
+        })
+
+        return NextResponse.json({
+            success: true,
+            ...aiOutput
+        })
+    } catch (error: any) {
+        console.error(error);
+        return NextResponse.json({
+            error: error.message || "Internal Error"
+        },
+            {
+                status: 500
+            })
+    }
+}
